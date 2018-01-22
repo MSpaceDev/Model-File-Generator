1a9531215d560546ecd1b18850e63930494d3860
MSpaceDev <mspacedev1@gmail.com>
2018-01-22 06:20:06 +0200

    Swapped over from Frames to Canvases for the ability to male a better looking program

--- Model Program.py
+++ Model Program.py
@@ -2,19 +2,11 @@ from tkinter import *
 
 class Block():
     def __init__(self, root):
-        self.frameBlock = Canvas(root, width=root.winfo_width(), height=root.winfo_height() / 2, highlightthickness=0)
-        self.frameBlock.grid_propagate(False)
-        self.frameBlock.grid(row=0)
+        background.create_text(100, 100, text="Hello")
 
 class Item():
     def __init__(self, root):
-        print(root.winfo_height() / 2)
-        self.frameItem = Canvas(root, width=root.winfo_width(), height=root.winfo_height() / 2, highlightthickness=0, bg="red")
-        self.frameItem.grid_propagate(False)
-        self.frameItem.grid(row=1)
-
-        self.label = Button(self.frameItem, text="Item")
-        self.label.grid(row=1, column=10)
+        pass
 
 root = Tk()
 
@@ -22,6 +14,12 @@ root.title("Minecraft Model Generator")
 root.geometry("1024x640")
 root.resizable(False, False)
 root.update()
+
+# Create background Image
+backgroundImg = PhotoImage(file="bg_main.png")
+background = Canvas(root, width=root.winfo_width(), height=root.winfo_height(), highlightthickness=0)
+background.create_image(0, 0, anchor=NW, image=backgroundImg)
+background.pack()
 
 # Load Separate GUI Frames
 Block(root)
