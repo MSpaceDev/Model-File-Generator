1c00cca5188b71eede233d553bccdebf4a8423c5
MSpaceDev <mspacedev1@gmail.com>
2018-01-23 23:49:55 +0200

    Finalised program layout and started on text fields; Added font used

--- Model Program.py
+++ Model Program.py
@@ -1,24 +1,60 @@
 from tkinter import *
 
+class CreateField():
+    def __init__(self, name, x, y):
+        self.name = name + ":"
+        background.create_text(padX + x, padY + y - 3, text=self.name, font="Neoteric 15", fill="#9ababc", anchor=NW)
+        self.entry = Entry(background, bg="#2f3136", fg="#9ababc", insertbackground="white")
+        background.create_window(padX + x + 130, padY + y, window=self.entry, anchor=NW)
+
+    def getValue(self):
+        return self.entry.get()
+
 class Block():
     def __init__(self, root):
-        background.create_text(100, 100, text="Hello")
+        background.create_text(mainWidth / 2, padY / 2, text="MINECRAFT MODEL GENERATOR", font="Neoteric 30", fill="#9ababc", anchor=CENTER)
+        background.create_rectangle(padX, padY, padX + length, padY + height, fill="#36393e", width=1)
+        background.create_text(padX + 10, padY, text="Block Generator", font="Neoteric 20", fill="#808387", anchor=NW)
+
+        self.modelNameEntry = CreateField("Model Name", 10, 50)
+        self.textureNameEntry = CreateField("Texture Name", 10, 80)
+
+        self.button = Button(background, text="Test", command=lambda: self.printEntry())
+        background.create_window(400, 400, window=self.button)
+
+    def printEntry(self):
+        if(self.modelNameEntry.getValue() == "stone"):
+            print("Is stone!!y")
 
 class Item():
     def __init__(self, root):
-        pass
+        startX = padX
+        startY = padY*2 + height
+        background.create_rectangle(startX, startY, padX + length, padY*2 + height*2, fill="#36393e", width=1)
+        background.create_text(startX + 10, startY, text="Item Generator", font="Neoteric 20", fill="#808387", anchor=NW)
 
+
+# Main Program
 root = Tk()
 
+height = 250
+padX = 50
+padY = 50
+sizeY = str(padY*3 + height*2)
+size = str(1024)+"x"+sizeY
+
 root.title("Minecraft Model Generator")
-root.geometry("1024x640")
+root.geometry(size)
 root.resizable(False, False)
 root.update()
 
-# Create background Image
-backgroundImg = PhotoImage(file="bg_main.png")
-background = Canvas(root, width=root.winfo_width(), height=root.winfo_height(), highlightthickness=0)
-background.create_image(0, 0, anchor=NW, image=backgroundImg)
+# Setup global static variables
+mainWidth = root.winfo_width()
+mainHeight = root.winfo_height()
+length = mainWidth - padX*2
+
+# Create background
+background = Canvas(root, width=mainWidth, height=mainHeight, highlightthickness=0, bg="#2f3136")
 background.pack()
 
 # Load Separate GUI Frames
